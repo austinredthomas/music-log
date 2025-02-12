5d81e31109c9424977c0e85882e756337e976b8b
Austin Thomas <107638329+austinredthomas@users.noreply.github.com>
2025-02-12 16:29:03 -0500

    feat: added getAlbum()

--- music-log.py
+++ music-log.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 # Get valid access token from Spotify API
 def requestToken(clientID, clientSecret):
@@ -18,24 +17,41 @@ def requestToken(clientID, clientSecret):
     return token
 
 def searchArtist(token):
+    q = input("Enter artist name: ")
     url = "https://api.spotify.com/v1/search"
     # Create header (Authorization, Content-Type)
     headers = {
         'Authorization': 'Bearer  ' + token,
     }
     params = {
-        'q': 'muse',
+        'q': q,
         'type': 'artist'
     }
     response = requests.get(url, headers=headers, params=params)
     jsonResponse = response.json()
-    print(jsonResponse['artists']['items'])
-    for jsonResponse['artists']['items']:
+    print(jsonResponse['artists']['items'][0]['name'])
+    for i in jsonResponse['artists']['items']:
+        artistName = i['name']
+        print("Is this your artist?: ", artistName)
+        answer = input("(y/n)")
+        if answer == "y" or answer == "Y":
+            return i['id']
 
-
-def getAlbum():
-    print("Getting album...")
-
+def getAlbum(artistID, token):
+    print("Getting ArtistID: ", artistID, " albums...") 
+    url = 'https://api.spotify.com/v1/artists/' + artistID + '/albums'
+    print(url)
+    headers = {
+        'Authorization': 'Bearer  ' + token
+    }
+    params = {
+        'include_groups': 'album'
+    }
+    response = requests.get(url, headers = headers, params = params) # url, headers, params
+    jsonResponse = response.json()
+    print("Response: ", jsonResponse['items'][0])
+    for i in jsonResponse['items']:
+        print("Album name: ", i['name'], "| Tracks: ", i['total_tracks'])
 if __name__ == '__main__':
     # get Client ID
     f = open("clientID.txt", "r")
@@ -52,6 +68,7 @@ if __name__ == '__main__':
     
     print("Token: ", token)
     
-    searchArtist(token)
+    artistID = searchArtist(token)
+    print("The artist you chose ID: ", artistID)
     
-    getAlbum()
+    getAlbum(artistID, token)
